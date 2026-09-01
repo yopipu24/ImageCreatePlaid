7adfbf1686284918f5dc6bdec6381d22019aaaca
yopipu <yopipu24@gmail.com>
2026-09-02 02:49:11 +0900

    modify DottedLineRandom, add sampleimage

--- Logic/EditScenery/DottedLineRandom.cs
+++ Logic/EditScenery/DottedLineRandom.cs
@@ -1,54 +1,99 @@
 ﻿using SkiaSharp;
 
-namespace ImageCreatePlaid
-{
-    public class DottedLineRandom : SceneryInterface
-    {
-        public SKBitmap EditImage(SKBitmap bmp, SceneryModel model)
-        {
-            int width = bmp.Width;
-            int height = bmp.Height;
-            int px = BussinessLogic.GetRandomInt(model.HorizontalSize1);
-            int py = BussinessLogic.GetRandomInt(model.VerticalSize1);
-            int px2 = BussinessLogic.GetRandomInt(model.HorizontalSize1);
-            int py2 = BussinessLogic.GetRandomInt(model.VerticalSize1);
-            SKColor basecolor = new SKColor(model.BaseColorRed, model.BaseColorGreen, model.BaseColorBlue, model.BaseAlpha);
-            SKColor h1color = new SKColor(model.HorizontalColorRed1, model.HorizontalColorGreen1, model.HorizontalColorBlue1, model.Alpha);
-            SKColor v1color = new SKColor(model.VerticalColorRed1, model.VerticalColorGreen1, model.VerticalColorBlue1, model.Alpha);
-            SKColor h2color = new SKColor(model.HorizontalColorRed2, model.HorizontalColorGreen2, model.HorizontalColorBlue2, model.Alpha);
-            SKColor v2color = new SKColor(model.VerticalColorRed2, model.VerticalColorGreen2, model.VerticalColorBlue2, model.Alpha);
+namespace ImageCreatePlaid;
 
-            int[] hlines = BussinessLogic.GetDotPattern(model.VerticalDottedPattern, model.HorizontalSize1);
-            int[] vlines = BussinessLogic.GetDotPattern(model.HorizontalDottedPattern, model.VerticalSize1);
+public class DottedLineRandom : SceneryInterface
+{
+    public SKBitmap EditImage(SKBitmap bmp, SceneryModel model)
+    {
+        int width = bmp.Width;
+        int height = bmp.Height;
+
+        int horizontalSize = model.HorizontalSize1;
+        int verticalSize = model.VerticalSize1;
+
+        int px = BussinessLogic.GetRandomInt(horizontalSize);
+        int py = BussinessLogic.GetRandomInt(verticalSize);
+        int px2 = BussinessLogic.GetRandomInt(horizontalSize);
+        int py2 = BussinessLogic.GetRandomInt(verticalSize);
+
+        var baseColor = new SKColor(
+            model.BaseColorRed, model.BaseColorGreen, model.BaseColorBlue, model.BaseAlpha);
+        var h1Color = new SKColor(
+            model.HorizontalColorRed1, model.HorizontalColorGreen1, model.HorizontalColorBlue1, model.Alpha);
+        var v1Color = new SKColor(
+            model.VerticalColorRed1, model.VerticalColorGreen1, model.VerticalColorBlue1, model.Alpha);
+        var h2Color = new SKColor(
+            model.HorizontalColorRed2, model.HorizontalColorGreen2, model.HorizontalColorBlue2, model.Alpha);
+        var v2Color = new SKColor(
+            model.VerticalColorRed2, model.VerticalColorGreen2, model.VerticalColorBlue2, model.Alpha);
+
+        int[] hlines = BussinessLogic.GetDotPattern(
+            model.VerticalDottedPattern, horizontalSize);
+        int[] vlines = BussinessLogic.GetDotPattern(
+            model.HorizontalDottedPattern, verticalSize);
+
+        int horizontalRange = horizontalSize / 100 + model.HorizontalDottedWeight;
+        int verticalRange = verticalSize / 100 + model.VerticalDottedWeight;
+
+        // X/Y ごとの判定を前計算する
+        var isHLine = new bool[width];
+        var isH1Area = new bool[width];
+        var isH2Area = new bool[width];
+
+        for (int x = 0; x < width; x++)
+        {
+            int localX = x % horizontalSize;
+
+            isHLine[x] = hlines[localX] == 1;
+            isH1Area[x] = localX > px - horizontalRange &&
+                          localX < px + horizontalRange;
+            isH2Area[x] = localX > px2 - horizontalRange &&
+                          localX < px2 + horizontalRange;
+        }
+
+        var isVLine = new bool[height];
+        var isV1Area = new bool[height];
+        var isV2Area = new bool[height];
+
+        for (int y = 0; y < height; y++)
+        {
+            int localY = y % verticalSize;
+
+            isVLine[y] = vlines[localY] == 1;
+            isV1Area[y] = localY > py - verticalRange &&
+                          localY < py + verticalRange;
+            isV2Area[y] = localY > py2 - verticalRange &&
+                          localY < py2 + verticalRange;
+        }
+
+        // 配列をまとめて設定する
+        var pixels = new SKColor[width * height];
+
+        for (int y = 0; y < height; y++)
+        {
+            int rowOffset = y * width;
 
             for (int x = 0; x < width; x++)
             {
-                for (int y = 0; y < height; y++)
-                {
-                    if (x % model.HorizontalSize1 > px2 - model.HorizontalSize1 / 100 - model.HorizontalDottedWeight && x % model.HorizontalSize1 < px2 + model.HorizontalSize1 / 100 + model.HorizontalDottedWeight && vlines[y % model.VerticalSize1] == 1)
-                    {
-                        bmp.SetPixel(x, y, h2color);
-                    }
-                    else if (y % model.VerticalSize1 > py2 - model.VerticalSize1 / 100 - model.VerticalDottedWeight && y % model.VerticalSize1 < py2 + model.VerticalSize1 / 100 + model.VerticalDottedWeight && hlines[x % model.HorizontalSize1] == 1)
-                    {
-                        bmp.SetPixel(x, y, v2color);
-                    }
-                    else if (x % model.HorizontalSize1 > px - model.HorizontalSize1 / 100 - model.HorizontalDottedWeight && x % model.HorizontalSize1 < px + model.HorizontalSize1 / 100 + model.HorizontalDottedWeight && vlines[y % model.VerticalSize1] == 1)
-                    {
-                        bmp.SetPixel(x, y, h1color);
-                    }
-                    else if (y % model.VerticalSize1 > py - model.VerticalSize1 / 100 - model.VerticalDottedWeight && y % model.VerticalSize1 < py + model.VerticalSize1 / 100 + model.VerticalDottedWeight && hlines[x % model.HorizontalSize1] == 1)
-                    {
-                        bmp.SetPixel(x, y, v1color);
-                    }
-                    else
-                    {
-                        bmp.SetPixel(x, y, basecolor);
-                    }
-                }
-            }
+                SKColor color;
 
-            return bmp;
+                if (isH2Area[x] && isVLine[y])
+                    color = h2Color;
+                else if (isV2Area[y] && isHLine[x])
+                    color = v2Color;
+                else if (isH1Area[x] && isVLine[y])
+                    color = h1Color;
+                else if (isV1Area[y] && isHLine[x])
+                    color = v1Color;
+                else
+                    color = baseColor;
+
+                pixels[rowOffset + x] = color;
+            }
         }
+
+        bmp.Pixels = pixels;
+        return bmp;
     }
 }
